944444461758bc94f5876ccf6f4a3ae31d3192a3
Christian Gröling <contact@christiangroeling.de>
2023-11-28 19:22:31 +0100

    feat: Add right alignment to format placeholders

--- src/lib.rs
+++ src/lib.rs
@@ -312,6 +312,50 @@ impl Formatify {
         }
     }
 
+    fn process_format_right_placeholder<T: ParsingTask>(
+        &self,
+        context: &mut ParsingContext<'_, T::Item>,
+    ) {
+        if consume_expected_chars!(context, '(').is_none() {
+            T::error(context);
+            return;
+        }
+        skip_until_neg_char_match!(context, ' '); // consume whitespaces
+
+        let Some(decimal) = self.parse_decimal_number(context) else {
+            T::error(context);
+            return;
+        };
+
+        skip_until_neg_char_match!(context, ' '); // consume whitespaces
+
+        // Check if optional arguments are available
+        /*  if consume_expected_chars!(context, ',').is_some() {
+            skip_until_neg_char_match!(context, ' '); // consume whitespaces
+            let Some(literal) = gather_str_placeholder!(context) else {
+                T::error(context);
+                return;
+            };
+            skip_until_neg_char_match!(context, ' '); // consume whitespaces
+            context.iter.next(); // consume )
+            let arg: String = literal.into_iter().collect();
+
+            if arg.trim() == "trunc" {
+                context.format = OutputFormat::LeftAlignTrunc(decimal);
+                return;
+            }
+
+            T::error(context);
+        } else {*/
+        if consume_expected_chars!(context, ')').is_none() {
+            T::error(context);
+            return;
+        }
+
+        context.format = OutputFormat::RightAlign(decimal);
+        //  }
+    }
+
     fn process_placeholder<T: ParsingTask>(&self, context: &mut ParsingContext<'_, T::Item>) {
         let Some(ch) = context.iter.next() else {
             return;
@@ -323,6 +367,9 @@ impl Formatify {
             }
             '<' => {
                 self.process_format_left_placeholder::<T>(context);
+            }
+            '>' => {
+                self.process_format_right_placeholder::<T>(context);
             }
             'n' => {
                 T::process_char_placeholder(context, '\n');
@@ -590,6 +637,12 @@ mod tests_measure_lengths {
     );
 
     test!(
+        test_with_right_alignment_placeholder_and_shorter_returns_correct_length,
+        "Hallo %>(10)%(str4)xx",
+        vec![18usize, 10usize]
+    );
+
+    test!(
         test_with_left_alignment_placeholder_and_exact_length_value_returns_correct_length,
         "Hallo %<(10)%(str10)xx", // "Hallo 1234567890xx"
         vec![18usize, 10usize]
@@ -737,14 +790,32 @@ mod tests_replace_placeholders {
     );
 
     test!(
+        test_with_right_alignment_placeholder_and_shorter_value_pads_correctly,
+        "Hallo %>(10)%(str4)xx",
+        "Hallo       1234xx"
+    );
+
+    test!(
         test_with_left_alignment_placeholder_and_exact_length_value_keeps_it_unchanged,
         "Hallo %<(10)%(str10)xx",
         "Hallo 1234567890xx"
     );
 
     test!(
+        test_with_right_alignment_placeholder_and_exact_length_value_keeps_it_unchanged,
+        "Hallo %>(10)%(str10)xx",
+        "Hallo 1234567890xx"
+    );
+
+    test!(
         test_with_left_alignment_placeholder_and_longer_value_keeps_it_unchanged,
         "Hallo %<(10)%(str14)xx",
+        "Hallo 1234567890ABCDxx"
+    );
+
+    test!(
+        test_with_right_alignment_placeholder_and_longer_value_keeps_it_unchanged,
+        "Hallo %>(10)%(str14)xx",
         "Hallo 1234567890ABCDxx"
     );
 
--- src/output_format.rs
+++ src/output_format.rs
@@ -2,4 +2,5 @@ pub enum OutputFormat {
     None,
     LeftAlign(u32),
     LeftAlignTrunc(u32),
+    RightAlign(u32),
 }
